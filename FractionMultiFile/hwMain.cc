431785b69e0a29fe1b6f0400872b3f4be4b5bc65
Dov Kruger <Dov.Kruger@gmail.com>
2019-02-20 16:07:05 -0500

    MultipleFileFraction homework

---
+++ FractionMultiFile/hwMain.cc
@@ -0,0 +1,17 @@
+int main() {
+	const Fraction a(1,2); // 1/2
+	const Fraction b(3);   // 3/1
+	Fraction c;      // 0/1
+
+	Fraction d  = a + b;	// use friend
+	Fraction e  = a - b;	// use member operator
+	Fraction f = -a;			// use friend
+	Fraction g = -b;			// use member operator
+	Fraction h = a.add(b);
+	cout << d << '\n';
+	cout << e << '\n';
+	cout << f << '\n';
+	cout << g << '\n';
+	cout << h << '\n';
+	cout << Fraction::getCount() << '\n'; // how many
+}
