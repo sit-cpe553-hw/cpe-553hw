2baf0fa8d619037262e795a2b0131ca41a759da7
Dov Kruger <Dov.Kruger@gmail.com>
2019-03-02 22:25:01 -0500

    bitmap homeworks

--- Bitmap/main.cc
+++ Bitmap/main.cc
@@ -1,10 +1,34 @@
 #include "Bitmap.hh"
+/*
+	Create a class that has a 2-D array of uint32_t.  Each number represents a color
+	in RGB format (one byte for each of the guns)
 
+	You can write the color in hex. Each two digits are red, green and blue
+
+ */
 int main() {
-	Bitmap b; // static sized bitmap 300x200
+	Bitmap b; // static sized bitmap 300x200 with every pixel = black = 0
+	
 	//https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	/*
+		draw a line using Bresenham's algorithm. Set all pixels to red
+		(0xFF0000)
+	*/
 	b.line(0,0, 200,100, 0xFF0000); // Bresenham's algorithm
+
+	
 	b.rect(50,0,100,50, 0x00FF00); // draw a green rectangle top-left=(50,0) w=100, h=50
-	b.write("test.bmp");
+
+	b.horizLine(0, 150, 165, 0x0000FF); // draw a horizontal line from x=0 to x=150 at y=165
+	// color is blue
+	
+	b.vertLine(10, 120, 40, 0xFFFFFF);
+
+#if 0
+	// write is not yet part of this activity.
+	// it will be added
+	b.write("test.bmp"); // write a .bmp file
+	//(if we have not covered this in time, wait until next assignment for writing the file)
+#endif
 }
 	
--- DynamicBitmap/main.cc
+++ DynamicBitmap/main.cc
@@ -1,5 +1,30 @@
+/*
+This homework is the same as bitmap, but uses dynamic memory allocation.
+Allocate a pointer to width*height pixels.
+
+The key information you will need is to determine the correct location
+Given a pixel at (x,y) the location in the block of memory is y * width + x
+
+*/
 int main() {
 	Bitmap b(320,200);
+	cout << b(10, 15) << '\n'; // extract the pixel at y = 10, x = 15
 	b.line(0,0, 100,100, 0xFF0000);
+
+		//https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+	/*
+		draw a line using Bresenham's algorithm. Set all pixels to red
+		(0xFF0000)
+	*/
+	b.line(0,0, 200,100, 0xFF0000); // Bresenham's algorithm
+
+	
+	b.rect(50,0,100,50, 0x00FF00); // draw a green rectangle top-left=(50,0) w=100, h=50
+
+	b.horizLine(0, 150, 165, 0x0000FF); // draw a horizontal line from x=0 to x=150 at y=165
+	// color is blue
+	
+	b.vertLine(10, 120, 40, 0xFFFFFF);
+
 	b.write("test.bmp");
 }
